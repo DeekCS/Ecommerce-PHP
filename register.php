0e885397c5df7f06324c2a7f2e9678d91d5927f8
DeekCS <31720001079@std.bau.edu.jo>
2022-01-17 12:23:34 +0200

    done

---
+++ register.php
@@ -0,0 +1,48 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Registration system PHP and MySQL</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="header">
+    <h2>Register</h2>
+</div>
+
+<form id="register" method="post" action="register.php">
+    <?php include('errors.php'); ?>
+    <div class="input-group">
+        <label>Username
+        <input type="text" id="username" name="username" value="<?php echo $username; ?>">
+        </label>
+        <small class="username-error"></small>
+    </div>
+    <div class="input-group">
+        <label>Email
+        <input type="email" id="email" name="email" value="<?php echo $email; ?>">
+        </label>
+        <small class="email-error"></small>
+    </div>
+    <div class="input-group">
+        <label>Password
+        <input id="password" type="password" name="password_1">
+        </label>
+        <small class="password-error"></small>
+    </div>
+    <div class="input-group">
+        <label>Confirm password
+        <input id="password2" type="password" name="password_2">
+        </label>
+        <small class="password2-error"></small>
+    </div>
+    <div class="input-group">
+        <button id="submit" type="submit" class="btn" name="reg_user">Register</button>
+    </div>
+    <p>
+        Already a member? <a href="login.php">Sign in</a>
+    </p>
+</form>
+<script src="validate.js"></script>
+</body>
+</html>
---
+++ server.php
@@ -0,0 +1,87 @@
+<?php
+session_start();
+
+// initializing variables
+$username = "";
+$email    = "";
+$errors = array();
+
+try {
+  $db = new PDO('mysql:host=localhost;dbname=php', 'root', '');
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $username = $_POST['username'];
+  $email = $_POST['email'];
+  $password_1 = $_POST['password_1'];
+  $password_2 = $_POST['password_2'];
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { $errors[] = "Username is required"; }
+  if (empty($email)) { $errors[] = "Email is required"; }
+  if (empty($password_1)) { $errors[] = "Password is required"; }
+  if ($password_1 != $password_2) {
+    $errors[] = "The two passwords do not match";
+  }
+
+  // first check the database to make sure
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $result = $db->query($user_check_query);
+  $user = $result->fetch(PDO::FETCH_ASSOC);
+
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      $errors[] = "Username already exists";
+    }
+
+    if ($user['email'] === $email) {
+      $errors[] = "email already exists";
+    }
+  }
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO users (username, email, password)
+  			  VALUES('$username', '$email', '$password')";
+  	$db->exec($query);
+  	$_SESSION['username'] = $username;
+  	$_SESSION['success'] = "You are
+  	logged in";
+  	header('location: index.php');
+  }
+}
+
+if (isset($_POST['login_user'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  if (empty($username)) {
+  	$errors[] = "Username is required";
+  }
+  if (empty($password)) {
+  	$errors[] = "Password is required";
+  }
+
+  if (count($errors) == 0) {
+  	$password = md5($password);
+  	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+  	$results = $db->query($query);
+  	if ($results->rowCount() == 1) {
+  	  $_SESSION['username'] = $username;
+  	  $_SESSION['success'] = "You are now logged in";
+  	  header('location: index.php');
+  	}else {
+  		$errors[] = "Wrong username/password combination";
+  	}
+  }
+}
+
