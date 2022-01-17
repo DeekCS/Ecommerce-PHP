b2671fc0f89072af6e4c906d02b686eb041908ee
DeekCS <31720001079@std.bau.edu.jo>
2022-01-17 13:52:06 +0200

    update

--- server.php
+++ server.php
@@ -26,33 +26,39 @@ if (isset($_POST['reg_user'])) {
   if (empty($username)) { $errors[] = "Username is required"; }
   if (empty($email)) { $errors[] = "Email is required"; }
   if (empty($password_1)) { $errors[] = "Password is required"; }
-  if ($password_1 != $password_2) {
+  if ($password_1 !== $password_2) {
     $errors[] = "The two passwords do not match";
   }
 
   // first check the database to make sure
   // a user does not already exist with the same username and/or email
   $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
-  $result = $db->query($user_check_query);
-  $user = $result->fetch(PDO::FETCH_ASSOC);
-
-  if ($user) { // if user exists
-    if ($user['username'] === $username) {
-      $errors[] = "Username already exists";
+    if (isset($db)) {
+        $result = $db->query($user_check_query);
+    }
+    if (isset($result)) {
+        $user = $result->fetch(PDO::FETCH_ASSOC);
     }
 
-    if ($user['email'] === $email) {
-      $errors[] = "email already exists";
+    if (isset($user) && $user) { // if user exists
+      if ($user['username'] === $username) {
+        $errors[] = "Username already exists";
+      }
+
+      if ($user['email'] === $email) {
+        $errors[] = "email already exists";
+      }
     }
-  }
 
   // Finally, register user if there are no errors in the form
-  if (count($errors) == 0) {
+  if (count($errors) === 0) {
   	$password = md5($password_1);//encrypt the password before saving in the database
 
   	$query = "INSERT INTO users (username, email, password)
   			  VALUES('$username', '$email', '$password')";
-  	$db->exec($query);
+      if (isset($db)) {
+          $db->exec($query);
+      }
   	$_SESSION['username'] = $username;
   	$_SESSION['success'] = "You are
   	logged in";
@@ -71,11 +77,11 @@ if (isset($_POST['login_user'])) {
   	$errors[] = "Password is required";
   }
 
-  if (count($errors) == 0) {
+  if (count($errors) === 0) {
   	$password = md5($password);
   	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
   	$results = $db->query($query);
-  	if ($results->rowCount() == 1) {
+  	if ($results->rowCount() === 1) {
   	  $_SESSION['username'] = $username;
   	  $_SESSION['success'] = "You are now logged in";
   	  header('location: index.php');
